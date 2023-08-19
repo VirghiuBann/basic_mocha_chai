8c04e4d6f5bf45603e77d8200eeb33c24a9e9083
virghiu <virghiu@gmail.com>
2023-08-19 16:15:48 -0400

    feat: complete test Famous Italian Explorers

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -71,6 +71,7 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+//Browser.site = '<repl URL>';
 Browser.site = 'http://localhost:3000';
 const browser = new Browser();
 
@@ -91,15 +92,28 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
 
-      done();
+        });
+      });
     });
   });
 });
